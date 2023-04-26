291f1febc1a8580d4141fd7eba886e3fd3cdd454
jaimebw <jaimebwv@gmail.com>
2023-04-25 23:03:44 -0400

    Changes for multiple threads and in UI

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ basedir = Path(__file__).resolve().parent
 class Config(object):
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            'sqlite:///'+ str(basedir/'app.db')
+            'sqlite:///'+ str(basedir/'app.db')+'?check_same_thread=False'
     SECRET_KEY = os.environ.get('SECRET_KEY') or "loco-loco"
-
+    
     FOAM_DIR = "/openfoam" # this dir is the one in the docker machine
