cc1abd3e4f077f28b8f80dcab977c901ba655a32
jaimebw <jaimebwv@gmail.com>
2023-08-13 11:29:09 -0400

    Deleted unused libs and functions.

--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,6 @@
-from pathlib import Path
-import zipfile
-import asyncio
-import json
 from app import app
-import os
-from io import BytesIO
+from pathlib import Path
+import asyncio
 
 
 def generate_id(name) -> str:
@@ -28,61 +24,6 @@ def is_systemfile(file_name)->bool:
         return True
     else:
         return False
-
-
-def get_zip_directory_structure(zip_file_path: str):
-    """
-    Gets the directory structure from a zip file and returns it as a JSON object
-    in the format expected by jsTree. Ignores .DS_Store files and __MACOSX directories.
-
-    NOTES:
-        This should be done in the client side, not in the server side.
-        Simfiles have their path as one of the columns of the db
-        It should be easy to get the directory structure from there.
-    TODO:
-        * Test this on Linux
-
-    PARAMETERS
-    ----------
-    zip_file_path: paht to the zip file
-
-    """
-    with zipfile.ZipFile(zip_file_path, "r") as zip_file:
-        zip_contents = zip_file.namelist()
-    nodes = []
-    for item in zip_contents:
-        if item.endswith(".DS_Store") or item.startswith("__MACOSX"):
-            continue
-        path = Path(item)
-        if path.parents[0].name == "":
-            node = {"id": generate_id(path.name), "parent": "#", "text": path.name}
-        else:
-            node = {
-                "id": generate_id(path.name),
-                "parent": generate_id(path.parents[0].name),
-                "text": path.name,
-            }
-        nodes.append(node)
-    return json.dumps(nodes)
-
-
-async def zip_dir(path):
-    """
-    Creates a ZIP archive of all the files in a directory and returns a BytesIO buffer containing the archive.
-
-    PARAMETERS
-    ----------
-    path: str - the path to the directory
-    returns: BytesIO - a buffer containing the contents of the ZIP archive
-    """
-    buffer = BytesIO()
-    with zipfile.ZipFile(buffer, "w") as zip_file:
-        for root, dirs, files in os.walk(path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zip_file.write(file_path, os.path.relpath(file_path, path))
-
-    return buffer.getvalue()
 
 
 async def run_command(command_list):
