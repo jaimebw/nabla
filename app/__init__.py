aeee58251f7254c30d0a9c10cd32c72a0499cc6e
jaimebw <jaimebwv@gmail.com>
2023-08-13 11:25:03 -0400

    Added SQLITE cascading.

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap5
 import os
-
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 
 # ENV variables
 
@@ -20,4 +21,13 @@ login = LoginManager(app)
 login.login_view = "login"
 bootstrap = Bootstrap5(app)
 
+@event.listens_for(Engine, "connect") # this is for cascading in sqlite
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
 from app import routes, models, errors
+
+
